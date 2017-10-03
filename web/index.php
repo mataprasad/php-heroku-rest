413ad606975fd325c95c050880b1ab4f6aa447ad
Mata Prasad Chauhan <mataprasad045@gmail.com>
2017-10-03 16:41:21 +0530

    init

---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+require('../vendor/autoload.php');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+$app->post('/api/register', function (Request $request) use ($app) {
+    $post = array(
+        'title' => $request->request->get('title'),
+        'body'  => $request->request->get('body'),
+    );
+
+    $post['id'] = rand();
+
+    return $app->json($post, 201);
+});
+
+
+$app->run();
